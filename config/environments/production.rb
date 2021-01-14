2bf1d3ee27747d8ab5b8a6b495e19f72c98dba01
Metal <ted.thetnaungsoe@gmail.com>
2021-01-14 19:24:35 +0700

    enable async

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # config.active_job.queue_adapter = GoodJob::Adapter.new(execution_mode: :async, max_threads: 5, poll_interval: 30)
+  config.active_job.queue_adapter = GoodJob::Adapter.new(execution_mode: :async, max_threads: 5, poll_interval: 30)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
